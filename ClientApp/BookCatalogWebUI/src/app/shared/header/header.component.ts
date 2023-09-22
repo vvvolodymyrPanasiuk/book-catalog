adef33127f3e608635a17c4ab311f26b5522b416
VolodymyrPanasiuk <vovkawool@gmail.com>
2023-09-22 19:40:41 +0300

    Implementation searh service into header component

--- ClientApp/BookCatalogWebUI/src/app/shared/header/header.component.ts
+++ ClientApp/BookCatalogWebUI/src/app/shared/header/header.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+
+import { Book } from 'src/app/core/models/book';
+import { SearchService } from 'src/app/core/services/search.service';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +11,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  constructor() { }
+
+  query?: string = "";
+  private searchTerms = new Subject<string>();
+  books$: Observable<Book[]> | undefined;
+  //showBooksDropdown = false;
+  booksList: Book[] = [];
+
+
+  constructor(private searchService: SearchService) { }
+
   ngOnInit(): void {
+    this.books$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((event: string) => {
+        if (event.trim() === '') {
+          // Повернення порожнього списку, якщо поле пошуку порожнє
+          return [];
+        } else {
+          console.log(this.searchService.searchBooks(event));
+          return this.searchService.searchBooks(event);
+        }
+      })
+    );
+  }
+
+  search(event: Event): void {
+    console.log((event.target as HTMLInputElement).value);
+    this.query = (event.target as HTMLInputElement).value;
+    //this.showBooksDropdown = this.query.length > 0;
+    this.searchTerms.next(this.query);
+  }
+
+  selectBook(book: Book): void {
+    // Виконайте дії, коли обраний певний елемент
+    console.log('Selected Book:', book);
+    this.query = book.title; // Встановіть вибраний текст у полі пошуку
+    this.booksList = []; // Очистіть список книжок
+
+    //this.showBooksDropdown = false; // Сховайте випадаючий список
   }
 }
