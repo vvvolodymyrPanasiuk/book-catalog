86e06eec3678e6f55f6a52fd0e41238ffc522095
VolodymyrPanasiuk <vovkawool@gmail.com>
2023-09-22 22:05:55 +0300

    Implementation SortBooksAsync metod for DAL layer

--- BookCatalog.DAL.EF/Repositories/BookRepository/BookRepository.cs
+++ BookCatalog.DAL.EF/Repositories/BookRepository/BookRepository.cs
@@ -21,5 +21,27 @@ namespace BookCatalog.DAL.EF.Repositories.BookRepository
                 .Where(book => book.Title.Contains(title))
                 .ToListAsync();
         }
+
+        public async Task<IEnumerable<Book>> SortBooksAsync(string sortBy, bool ascending)
+        {
+            var query = _dbSet.AsQueryable();
+
+            switch (sortBy.ToLower())
+            {
+                case "title":
+                    query = ascending ? query.OrderBy(b => b.Title) : query.OrderByDescending(b => b.Title);
+                    break;
+                case "publicationdate":
+                    query = ascending ? query.OrderBy(b => b.PublicationDate) : query.OrderByDescending(b => b.PublicationDate);
+                    break;
+                case "pagecount":
+                    query = ascending ? query.OrderBy(b => b.PageCount) : query.OrderByDescending(b => b.PageCount);
+                    break;
+                default:
+                    break;
+            }
+
+            return await query.ToListAsync();
+        }
     }
 }
